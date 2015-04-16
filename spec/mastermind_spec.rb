e9fa95ff75aa518067dbc706a33c3e6becba173b
Adam Smith <adamsmith910@aol.com>
2015-04-16 01:05:50 -0600

    started first test

--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -1,7 +1,11 @@
+require "rspec"
 require_relative "../lib/mastermind.rb"
 
-describe MasterMind do
 
+describe MasterMind do
   before { @mastermind = MasterMind.new }
 
+  describe "it initializes with turns" do
+    expect(@mastermind.turns).to eq(0)
+  end   
 end
